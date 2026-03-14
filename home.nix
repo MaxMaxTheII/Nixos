72a88aeb6d0f621066f5565f663845e8b7dfc027
Max Schüßler <max.schuessler@tugraz.at>
2026-03-14 18:32:35 +0100

    update: $(date)

--- home.nix
+++ home.nix
@@ -16,6 +16,23 @@
   # This allows Home Manager to manage itself
   programs.home-manager.enable = true;
 
+  # ----- cursor ---
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
+
+gtk = {
+  enable = true;
+  cursorTheme = {
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+  };
+};
+
   # --- KITTY CONFIGURATION ---
   programs.kitty = {
     enable = true;
@@ -53,6 +70,11 @@ wayland.windowManager.hyprland = {
     };
     exec-once = [
       "waybar"
+      "hyprctl setcursor Bibata-Modern-Ice 24"
+    ];
+    env = [
+      "XCURSOR_SIZE,24"
+      "HYPRCURSOR_SIZE,24"
     ];
     monitor = ",preferred,auto,1";
     general = {
