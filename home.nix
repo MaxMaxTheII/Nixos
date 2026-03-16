f3d1b5501fcbb1f005fc2bbdea35f54d66508d49
Max Schüßler <max.schuessler@tugraz.at>
2026-03-16 11:48:15 +0100

    update: Mo 16 Mär 2026 11:48:15 CET

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@
   # ----- cursor ---
   home.pointerCursor = {
     gtk.enable = true;
-    # x11.enable = true;
+    x11.enable = true;
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
     size = 24;
