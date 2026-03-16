aad1ae644f7d2c0487cf209fa71ea57dc67ffb8d
Max Schüßler <max.schuessler@tugraz.at>
2026-03-16 14:55:43 +0100

    update: Mo 16 Mär 2026 14:55:43 CET

--- home.nix
+++ home.nix
@@ -205,7 +205,8 @@ wayland.windowManager.hyprland = {
 services.hyprpaper = {
   enable = true;
   settings = {
-  preload = [(import ./data/variables.nix var).paths.wallpaper]; #[ "/home/max/dotfiles/data/wallpapers/zergling-wp.png" ];
+#  preload = [(import ./data/variables.nix var).paths.wallpaper]; #[ "/home/max/dotfiles/data/wallpapers/zergling-wp.png" ];
+  preload = [ "/home/max/dotfiles/data/wallpapers/zergling-wp.png" ];
     wallpaper = [ ",/home/max/dotfiles/data/wallpapers/zergling-wp.png" ];
   };
 };
