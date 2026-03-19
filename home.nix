80bc961786e23e26494a19290826b9b9ab797dd4
Max Schüßler <max.schuessler@tugraz.at>
2026-03-19 12:10:52 +0100

    update: Do 19 Mär 2026 12:10:52 CET

--- home.nix
+++ home.nix
@@ -161,7 +161,7 @@ wayland.windowManager.hyprland = {
       "SUPER, V, togglefloating,"
       "SUPER, L, exec, hyprlock"
       "SUPER, P, exec, hyprshot -z -m region -o ~/Pictures/screenshots/"
-      "SUPER SHILF, P, exec, hyprshot --clipboard-only -z -m region"
+      "SUPER SHIFT, P, exec, hyprshot --clipboard-only -z -m region"
       #"SUPER, R, exec, wofi --show drun"
       # Focus movement
       "SUPER, left, movefocus, l"
