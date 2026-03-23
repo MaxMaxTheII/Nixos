3853a6d079440b24f9c059f06c9f2e97b79ef0d2
Max Schüßler <max.schuessler@tugraz.at>
2026-03-23 15:01:10 +0100

    update: Mo 23 Mär 2026 15:01:10 CET

--- stylix.nix
+++ stylix.nix
@@ -3,7 +3,7 @@
         enable = true;
         image = ./data/wallpapers/zergling-wp.png;
         # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";# TODO remove
-        base16Scheme = lib.attrsets.recursiveUpdate 
+/*        base16Scheme = lib.attrsets.recursiveUpdate 
             (config.lib.stylix.scheme "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml")
             {
             base00 = "18111b"; # A very deep purple-black (Darker than Mocha)
@@ -13,6 +13,25 @@
             base0D = "f5c2e7"; # The "Zergling Pink" for active window borders/highlights
             base0E = "cba6f7"; # Soft Purple for keywords
             };
+            */ 
+        base16Scheme = {
+            base00 = "18111b"; # Deep dark purple-black background
+            base01 = "211924"; # Lighter background (status bars)
+            base02 = "3b2d3d"; # Selection background
+            base03 = "8c6c8c"; # Line Numbers / Comments (Visible but subtle pink-gray)
+            base04 = "b48ead"; # Subtle Muted Pink
+            base05 = "f5e0dc"; # Main Text (Soft Rose-White)
+            base06 = "f2cdcd"; # Brighter foreground
+            base07 = "b4befe"; # Soft lavender
+            base08 = "f38ba8"; # Red (Errors)
+            base09 = "fab387"; # Orange
+            base0A = "f9e2af"; # Yellow
+            base0B = "a6e3a1"; # Green
+            base0C = "94e2d5"; # Cyan
+            base0D = "f5c2e7"; # Zergling Pink (Active Border / Highlights)
+            base0E = "cba6f7"; # Soft Purple
+            base0F = "f2cdcd"; # Extra pink accent
+        }; 
         fonts = {
             monospace = {
                 package = pkgs.comic-mono;
