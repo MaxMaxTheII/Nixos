38e301c136d19bfd9845feb995ae59c5f9c8f8e1
Max Schüßler <max.schuessler@tugraz.at>
2026-03-23 14:58:14 +0100

    update: Mo 23 Mär 2026 14:58:14 CET

--- stylix.nix
+++ stylix.nix
@@ -2,8 +2,17 @@
     stylix = {
         enable = true;
         image = ./data/wallpapers/zergling-wp.png;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";# TODO remove
-
+        # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";# TODO remove
+        base16Scheme = lib.attrsets.recursiveUpdate 
+            (config.lib.stylix.scheme "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml")
+            {
+            base00 = "18111b"; # A very deep purple-black (Darker than Mocha)
+            base03 = "8c6c8c"; # Brighter pinkish-gray for Line Numbers/Comments
+            base04 = "b48ead"; # Subtle Muted Pink for secondary UI
+            base05 = "f5e0dc"; # Soft Rose-White for main text
+            base0D = "f5c2e7"; # The "Zergling Pink" for active window borders/highlights
+            base0E = "cba6f7"; # Soft Purple for keywords
+            };
         fonts = {
             monospace = {
                 package = pkgs.comic-mono;
@@ -26,6 +35,7 @@
         opacity = {
             terminal = 0.8;
             applications = 0.8;
+            desktop = 0.7;
         };
     };
     fonts.packages = with pkgs; [
