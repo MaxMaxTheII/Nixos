82401b6f144be21e06e02b37d31fa40b70984ba3
Max Schüßler <max.schuessler@tugraz.at>
2026-03-23 12:17:32 +0100

    update: Mo 23 Mär 2026 12:17:32 CET

--- stylix.nix
+++ stylix.nix
@@ -1,35 +1,35 @@
 { pkgs, ... }: {
-  stylix = {
-    enable = true;
-    image = ./data/wallpapers/zergling-wp.png;
+    stylix = {
+        enable = true;
+        image = ./data/wallpapers/zergling-wp.png;
 
-    stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";# TODO remove
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";# TODO remove
     
-    fonts = {
-      monospace = {
-        package = pkgs.comic-mono;
-        name = "Comic Mono";
-      };
-      sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
-      };
-      serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
-      };
+        fonts = {
+            monospace = {
+                package = pkgs.comic-mono;
+                name = "Comic Mono";
+            };
+            sansSerif = {
+                package = pkgs.dejavu_fonts;
+                name = "DejaVu Sans";
+            };
+            serif = {
+                package = pkgs.dejavu_fonts;
+                name = "DejaVu Serif";
+            };
+            sizes = {
+                applications = 12;
+                terminal = 14;
+            };
+        };
 
-      sizes = {
-        applications = 12;
-        terminal = 14;
-      };
+        opacity = {
+            terminal = 0.8;
+            applications = 0.8;
+        };
     };
-  };
-
-  opacity.terminal = 0.8;
-  opacity.applications = 0.8;
-
-  fonts.packages = with pkgs; [
-    font-awesome
-  ];
+    fonts.packages = with pkgs; [
+        font-awesome
+    ];
 }
