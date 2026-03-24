d377c60b1fd7c1f9aa17adb0239d39427ae5601e
Max Schüßler <max.schuessler@tugraz.at>
2026-03-24 17:54:24 +0100

    update: Di 24 Mär 2026 17:54:24 CET

--- nvim.nix
+++ nvim.nix
@@ -16,7 +16,18 @@ programs.zathura = {
       recolor-lightcolor = "#1e1e1e";
       recolor-darkcolor = "#d4d4d4";*/
     };
-  };
+    settings = {
+        opener = {
+            pdf-viewer = [
+                { run = ''zathura "$@"''; block = false; }
+            ];
+        };
+        open = {
+            rules = [
+                { mime = "application/pdf"; use = "pdf-viewer"; }
+            ];
+        };
+    };
   programs.neovim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
