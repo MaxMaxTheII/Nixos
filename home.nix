5357c31379815d8181658055fe0c8074646562fd
Max Schüßler <max.schuessler@tugraz.at>
2026-03-24 14:38:41 +0100

    update: Di 24 Mär 2026 14:38:41 CET

--- home.nix
+++ home.nix
@@ -91,6 +91,18 @@
             noDisplay = true;
 
         };
+        kvantummanager = {
+            name = "Kvantum Manager";
+            noDisplay = true;
+        };
+        qt5ct = {
+            name = "Qt5 Settings";
+            noDisplay = true;
+        };
+        qt6ct = {
+            name = "Qt6 Settings";
+            noDisplay = true;
+        };
     };
 
   # ----- cursor ---
