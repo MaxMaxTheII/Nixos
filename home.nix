fa17fedc4ba79a53f330ca05413dc3992bcef4cd
Max Schüßler <max.schuessler@tugraz.at>
2026-04-16 13:02:48 +0200

    update: Do 16 Apr 2026 13:02:48 CEST

--- home.nix
+++ home.nix
@@ -306,11 +306,11 @@
                         on-scroll     = 1;
                         on-click-right= "mode";
                         format = {
-                            months =     "<span color='${config.lib.stylix.colors.withHashtag.base0D}'><b>{}</b></span>";
-                            days =       "<span color='#ecc6d9'><b>{}</b></span>";
-                            weeks =      "<span color='#99ffdd'><b>W{}</b></span>";
-                            weekdays =   "<span color='#ffcc66'><b>{}</b></span>";
-                            today =      "<span color='#ff6699'><b><u>{}</u></b></span>";
+                            months =     "<span color='${config.lib.stylix.colors.withHashtag.base0E}'><b>{}</b></span>";
+                            days =       "<span color='${config.lib.stylix.colors.withHashtag.base0A}'><b>{}</b></span>";
+                            weeks =      "<span color='${config.lib.stylix.colors.withHashtag.base09}'><b>W{}</b></span>";
+                            weekdays =   "<span color='${config.lib.stylix.colors.withHashtag.base0B}$'><b>{}</b></span>";
+                            today =      "<span color='${config.lib.stylix.colors.withHashtag.base0C}'><b><u>{}</u></b></span>";
                         };
                     };
                 };
