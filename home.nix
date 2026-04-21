8887e951a0dff1b165126c501d70938e86eec9f9
Max Schüßler <max.schuessler@tugraz.at>
2026-04-21 11:09:14 +0200

    update: Di 21 Apr 2026 11:09:14 CEST

--- home.nix
+++ home.nix
@@ -456,7 +456,7 @@
                     { run = ''$EDITOR "$@"''; orphan = true; block = true;}
                 ];
                 open = [
-                    {run = ''xdg-open "%s1"'';}
+                    {run = ''xdg-open "$@"''; orphan = true;}
                 ];
             };
             open = {
