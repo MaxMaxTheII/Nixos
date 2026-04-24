bf5ccecd0754814ce84a83fd1991b5ec7754c00b
Max Schüßler <max.schuessler@tugraz.at>
2026-04-24 13:55:01 +0200

    update: Fr 24 Apr 2026 13:55:01 CEST

--- configuration.nix
+++ configuration.nix
@@ -3,14 +3,23 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-
+let
+    unstableTarball =
+        fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+in 
 {
     imports =
         [ # Include the results of the hardware scan.
             ./hardware-configuration.nix
             ./stylix.nix
         ];
-
+    nixpkgs.config = {
+        packageOverrides = pkgs: with pkgs; {
+            unstable = import unstableTarball {
+                config = config.nixpkgs.config;
+            };
+        };
+    };
   # Bootloader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
