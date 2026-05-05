20c21bd0d29e346859562f700e822996301cddbd
Max Schüßler <max.schuessler@tugraz.at>
2026-05-05 16:24:46 +0200

    update: Di 05 Mai 2026 16:24:46 CEST

--- home.nix
+++ home.nix
@@ -297,10 +297,10 @@
                 enabled = true;
                 bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
                 animation = [
-                    "windows, 1, 7, myBezier"
-                    "windowsOut, 1, 7, default, popin 80%"
+                    "windows, 1, 10, myBezier"
+                    "windowsOut, 1, 10, default, popin 80%"
                     "border, 1, 10, default"
-                    "fade, 1, 7, default"
+                    "fade, 1, 10, default"
                     "workspaces, 1, 10, default" # This controls the "Tab" speed between workspaces
                 ];
             };
