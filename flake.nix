dcd77e19d9242eb331346f541a5b5d1a84dab0b9
Max Schüßler <max.schuessler@tugraz.at>
2026-05-21 14:17:21 +0200

    update: Do 21 Mai 2026 14:17:21 CEST

--- flake.nix
+++ flake.nix
@@ -5,7 +5,11 @@
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
         stylix.url = "github:nix-community/stylix/release-25.11";
-    
+        elephant.url = "github:abenz1267/elephant";
+        walker = {
+            url = "github:abenz1267/walker";
+            inputs.elephant.follows = "elephant";
+        }; 
     # Add Home Manager input
         home-manager = {
             url = "github:nix-community/home-manager/release-25.11";
