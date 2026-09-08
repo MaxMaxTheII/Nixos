98105c203843ae99feb86255d178926aa6d6084b
Max Schüßler <max.schuessler@tugraz.at>
2026-09-08 12:08:24 +0200

    update: Di 08 Sep 2026 12:08:24 CEST

--- home.nix
+++ home.nix
@@ -346,45 +346,5 @@ in
     
     
     # --- BASH CONFIGURATION ---
-    programs.fastfetch = {
-        enable = true;
-        package = unstable.fastfetch;
-        settings = {
-            logo = {
-                color = {
-                    "1"= "${config.lib.stylix.colors.withHashtag.base0A}";
-                    "2"= "${config.lib.stylix.colors.withHashtag.base0B}";
-                    "3"= "${config.lib.stylix.colors.withHashtag.base0C}";
-                    "4"= "${config.lib.stylix.colors.withHashtag.base0D}";
-                    "5"= "${config.lib.stylix.colors.withHashtag.base0E}";
-                    "6"= "${config.lib.stylix.colors.withHashtag.base0F}";
-                };
-                source = "nixos";
-                padding.left = 2;
-            };
-            modules = [
-                "title"
-                "separator"
-                "os"
-                "host"
-                "kernel"
-                "uptime"
-#                "packages"
-                "shell"
-                "display"
-                "terminal"
-                "terminalfont"
-                "cpu"
-          #      "gpu"
-                "memory"
-          #      "swap"
-          #      "disk"
-                "battery"
-                "poweradapter"
-                "locale"
-                "colors"
-            ];
-        };
-    };
 }
 
