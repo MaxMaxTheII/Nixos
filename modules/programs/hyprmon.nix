b489a8eb5215d03994a0660633cf19b59363abb7
Max Schüßler <max.schuessler@tugraz.at>
2026-09-14 11:26:28 +0200

    update: Mo 14 Sep 2026 11:26:28 CEST

--- modules/programs/hyprmon.nix
+++ modules/programs/hyprmon.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}:
+let 
+    unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in 
 {
     home.packages = [
-        pkgs.hyprmon
+        unstable.hyprmon
     ];
 }
