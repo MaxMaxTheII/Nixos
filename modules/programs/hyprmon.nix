19a289fa3c3512e3c64736357d286720c7e43cc5
Max Schüßler <max.schuessler@tugraz.at>
2026-09-14 12:33:18 +0200

    update: Mo 14 Sep 2026 12:33:18 CEST

--- modules/programs/hyprmon.nix
+++ modules/programs/hyprmon.nix
@@ -6,7 +6,7 @@ in
     home.packages = [
         unstable.hyprmon
     ];
-    wayland.windowManager.hyprland.extraConfig = ''
+    wayland.windowManager.hyprland.extraConfig = lib.mkAfter ''
         require("hyprmon")
     '';
 }
